c3e713c46cce79d9f594ba9b94d9b77e573039cd
Alex Abatov <a.aliksant315@gmail.com>
2025-05-15 23:10:56 +0900

    just prepared files to code first components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div>Hello</div>;
+export default function App() {
+  return (
+    <div className="main">
+      <Header />
+      <Search />
+    </div>
+  );
 }
 
-export default App;
+function Header() {
+  return <h3 className="header">IP Address Tracker</h3>;
+}
+
+function Search() {
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search for any IP address or domain"
+    ></input>
+  );
+}
